be93743730841f665df5086e8c35833fc5447c0a
hernandez maikol <hernandez_maikol@protonmail.com>
2023-09-19 10:21:01 -0400

    Comment - components/ReplyButton - adding theme colors

--- src/components/Commnent/components/ReplyButton.jsx
+++ src/components/Commnent/components/ReplyButton.jsx
@@ -2,9 +2,9 @@ export default function ReplyButton({ commentDispatch }) {
   return(
     <button
       onClick={() => console.log("Show reply form")}
-      className="block font-bold capitalize text-purple-600 leading-10"
+      className="block font-bold capitalize text-moderate-blue leading-10"
     >
-      icon reply
+      reply
     </button>
   )
 }
