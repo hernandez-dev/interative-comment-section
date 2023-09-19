61d7a55f0269d2eb7250643b72a6e72862b739d6
hernandez maikol <hernandez_maikol@protonmail.com>
2023-09-19 17:30:45 -0400

    Comment - components/Replies - passing new prop to Comment ( currentUser )

--- src/components/Commnent/components/Replies.jsx
+++ src/components/Commnent/components/Replies.jsx
@@ -4,10 +4,10 @@ import Container from "../../Container.jsx"
 // components
 import Comment from "../Comment.jsx"
 
-export default function Replies({ replies }) {
+export default function Replies({ replies, currentUser }) {
   return(
     <Container styles="pl-4 mt-4 border-l-2 border-gray-300">
-      {replies.map(replie => <Comment key={replie.id} comment={replie} type="replie" />)}
+      {replies.map(replie => <Comment key={replie.id} comment={replie} type="replie" currentUser={currentUser} />)}
     </Container>
   )
 }
