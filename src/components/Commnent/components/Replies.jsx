c27dd9a7ab23db4fe727895a40d6e26755131941
hernandez maikol <hernandez_maikol@protonmail.com>
2023-09-19 10:21:30 -0400

    Comment - components/Replies - adding theme colors

--- src/components/Commnent/components/Replies.jsx
+++ src/components/Commnent/components/Replies.jsx
@@ -6,7 +6,7 @@ import Comment from "../Comment.jsx"
 
 export default function Replies({ replies }) {
   return(
-    <Container styles="pl-4 mt-4 space-y-2 border-l-2 border-gray-200">
+    <Container styles="pl-4 mt-4 border-l-2 border-gray-300">
       {replies.map(replie => <Comment key={replie.id} comment={replie} type="replie" />)}
     </Container>
   )
