6169a4abebf5ab8d75353cebba9f63d98741839f
hernandez maikol <hernandez_maikol@protonmail.com>
2023-09-19 17:16:32 -0400

    Comment - components/index.js - exporting component ( CommentActions )

--- src/components/Commnent/components/index.js
+++ src/components/Commnent/components/index.js
@@ -1,3 +1,4 @@
 export { default as User } from "./User.jsx"
 export { default as ReplyButton } from "./ReplyButton.jsx"
 export { default as Replies } from "./Replies.jsx"
+export { default as CommentActions } from "./CommentActions.jsx"
